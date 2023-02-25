8642d50bbfa070fc5048da82c0a7195c95a9787c
Jelou <arjelou.jelou>
2023-02-25 21:38:41 +0800

    about us

--- src/App.js
+++ src/App.js
@@ -3,22 +3,41 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+  <header className="hero">
+      <img src={logo}  alt="logo" />
+    <article>
+      <h1>Garments Manufacturing</h1>
+      <p>
+      The company prides itself on using only the best materials and employing skilled 
+      workers who are passionate about their craft. MAGSIGE offers a range of services, 
+      from design and prototyping to mass production and delivery.
+      </p>
+      <button type="button">Learn more...</button>
+    </article>
+  </header>
+  <section className='listItems'>
+    <article>
+      <h3>About Us</h3>
+      <p>Exercitation aute culpa magna voluptate mollit veniam Lorem dolore fugiat quis non commodo. Anim magna in fugiat cupidatat cupidatat aliquip. Sint tempor ipsum deserunt et ut. Cupidatat labore aute laboris sit qui. Id duis deserunt velit cillum ullamco laboris et do excepteur anim exercitation. Velit incididunt tempor do est aliquip et. Irure aute fugiat ad mollit Lorem in aliquip proident culpa.
+          Proident commodo ad id qui voluptate. Minim aliqua deserunt voluptate ut dolore cillum elit. Dolore laboris dolor dolore quis veniam proident id id. Est veniam aute amet ad nostrud.</p>
+    </article>
+    <ul className='vision'>
+      <li>
+        <h3>Vision 1</h3>
+        <p>Nostrud cupidatat irure cupidatat anim nulla fugiat quis veniam ipsum ad occaecat ut est. Cupidatat non dolor id elit cillum esse aliqua fugiat et ad laboris culpa est veniam. Qui reprehenderit et elit cupidatat tempor labore quis cupidatat minim adipisicing. Do officia tempor minim Lorem nisi ullamco veniam ut adipisicing dolor dolor. Dolore ipsum ullamco laboris laboris est qui cillum. Velit consequat consectetur ullamco voluptate laboris nulla velit tempor. Anim adipisicing Lorem deserunt amet ad voluptate incididunt magna irure cupidatat nulla deserunt.</p>
+      </li>
+      <li>
+        <h3>Vision 1</h3>
+        <p>Nostrud cupidatat irure cupidatat anim nulla fugiat quis veniam ipsum ad occaecat ut est. Cupidatat non dolor id elit cillum esse aliqua fugiat et ad laboris culpa est veniam. Qui reprehenderit et elit cupidatat tempor labore quis cupidatat minim adipisicing. Do officia tempor minim Lorem nisi ullamco veniam ut adipisicing dolor dolor. Dolore ipsum ullamco laboris laboris est qui cillum. Velit consequat consectetur ullamco voluptate laboris nulla velit tempor. Anim adipisicing Lorem deserunt amet ad voluptate incididunt magna irure cupidatat nulla deserunt.</p>
+      </li>
+      <li>
+        <h3>Vision 1</h3>
+        <p>Nostrud cupidatat irure cupidatat anim nulla fugiat quis veniam ipsum ad occaecat ut est. Cupidatat non dolor id elit cillum esse aliqua fugiat et ad laboris culpa est veniam. Qui reprehenderit et elit cupidatat tempor labore quis cupidatat minim adipisicing. Do officia tempor minim Lorem nisi ullamco veniam ut adipisicing dolor dolor. Dolore ipsum ullamco laboris laboris est qui cillum. Velit consequat consectetur ullamco voluptate laboris nulla velit tempor. Anim adipisicing Lorem deserunt amet ad voluptate incididunt magna irure cupidatat nulla deserunt.</p>
+      </li>
+    </ul>
+  </section>
+</>  
   );
 }
 
